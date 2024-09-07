024b845bf6787d277f66bf419c67f19f460db05e
Ravi Shankar Kumar <kumarravi199818@gmail.com>
2024-09-07 12:28:56 +0530

    Update solution.js

--- Day-1/solution.js
+++ Day-1/solution.js
@@ -1,1 +1,22 @@
+var twoSum = function(nums, target) {
+let twoSum = []
+for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<nums.length;j++){
+    if(nums[i] + nums[j] === target){
+        twoSum.push(i,j)
+        break;
+    }
+    }
+}
+console.log("Two Sum",twoSum);
 
+return twoSum;
+    
+};
+
+// Example input for testing
+let nums = [2, 7, 11, 15];
+let target = 9;
+
+// Call the function with the example input
+twoSum(nums, target);
